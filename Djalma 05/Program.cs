0fa24ae742ed0779abbad8c1cfb036d1e39bd53e
curso.ads <curso.ads@LAB06-71>
2022-07-06 19:30:21 -0300

    Adicionar arquivos de projeto.

---
+++ Djalma 05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djalma_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            for (n=1; n<=100; n++)
+                if(n%2 ==0)
+                {
+                    Console.WriteLine(n);
+                    }
+            Console.ReadKey();
+        }
+    }
+}
